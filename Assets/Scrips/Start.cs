720e31ad7167f75e1bd59f90497a3a20c989a840
Caletre <dharma.massenzio@estudiante.imagecampus.edu.ar>
2023-09-29 18:49:41 -0300

    //Condiciones Avanzadas

---
+++ Assets/Scrips/Start.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Backroom 2");
+    }
+
+    public void Exit()
+    {
+
+    }
+}
